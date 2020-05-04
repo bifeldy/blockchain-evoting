bbd3219094dc0c9df14c67a85bff7c92b3ef2728
Bifeldy <bias.astho@gmail.com>
2020-05-04 23:15:13 +0900

    🥰 Ethereum Smart Contract

--- contract/contracts/Ballot.sol
+++ contract/contracts/Ballot.sol
@@ -4,7 +4,6 @@ pragma solidity >=0.4.21 <0.7.0;
 contract Ballot {
 
   struct Voter {
-    string voterName;
     address voterAddress;
     uint voterWeight;
     bool voterVoted;
@@ -12,14 +11,12 @@ contract Ballot {
   }
 
   struct Candidate {
-    string candidateName;
     address candidateAddress;
     uint candidateVoteCount;
   }
 
   struct Election {
     string electionId;
-    string electionName;
     bool electionIsActive;
     address electionCreator;
     mapping(address => Candidate) electionCandidates;
@@ -42,11 +39,10 @@ contract Ballot {
 
   /// Registered User Show Their Voted Candidate Secretly
   function showMyVote(string memory _electionId, address _voterAddress) public view returns (
-    string memory voterName, address voterAddress, uint voterWeight, bool voterVoted, address voterSelectedCandidate
+    address voterAddress, uint voterWeight, bool voterVoted, address voterSelectedCandidate
   ) {
-    require(elections[_electionId].electionParticipants[_voterAddress].voterAddress == msg.sender, "This Is Not Your Ballot!");
+    require(_voterAddress == msg.sender, "This Is Not Your Ballot!");
     return (
-      elections[_electionId].electionParticipants[_voterAddress].voterName,
       elections[_electionId].electionParticipants[_voterAddress].voterAddress,
       elections[_electionId].electionParticipants[_voterAddress].voterWeight,
       elections[_electionId].electionParticipants[_voterAddress].voterVoted,
@@ -56,10 +52,9 @@ contract Ballot {
 
   /// View Current Candidate Voted Count
   function showResultCount(string memory _electionId, address _candidateAddress) public view returns (
-    string memory candidateName, address candidateAddress, uint candidateVoteCount
+    address candidateAddress, uint candidateVoteCount
   ) {
     return (
-      elections[_electionId].electionCandidates[_candidateAddress].candidateName,
       elections[_electionId].electionCandidates[_candidateAddress].candidateAddress,
       elections[_electionId].electionCandidates[_candidateAddress].candidateVoteCount
     );
@@ -68,31 +63,35 @@ contract Ballot {
   /* Setter */
 
   /// Admin Create New Election
-  function createElection(string memory _electionId, string memory _electionName) public {
+  function createElection(string memory _electionId) public {
     // require(contractOwner == msg.sender, "Only Contract Owner Can Create Election!");
     elections[_electionId].electionId = _electionId;
-    elections[_electionId].electionName = _electionName;
     elections[_electionId].electionCreator = msg.sender;
     elections[_electionId].electionIsActive = true;
   }
 
-  /// Admin Add New Candidate In Specific Election
-  function addCandidate(string memory _electionId, string memory _candidateName, address _candidateAddress) public {
+  /// Admin Add New Some Candidate In Specific Election
+  function addCandidate(string memory _electionId, address[] memory _candidateAddress) public {
     require(elections[_electionId].electionCreator == msg.sender, "Only Election Creator Can Add New Candidate!");
     require(elections[_electionId].electionIsActive, "Election Periode Has Ended!");
-    elections[_electionId].electionCandidates[_candidateAddress].candidateName = _candidateName;
-    elections[_electionId].electionCandidates[_candidateAddress].candidateAddress = _candidateAddress;
+    for (uint i = 0; i < _candidateAddress.length; i++) {
+      elections[_electionId].electionCandidates[_candidateAddress[i]].candidateAddress = _candidateAddress[i];
+    }
   }
 
   // Admin Add New Voter Participant In Specific Election And Give 1 Ballot
-  function addParticipant(string memory _electionId, string memory _voterName, address _voterAddress) public {
+  function addParticipant(string memory _electionId, address[] memory _voterAddress) public {
     require(elections[_electionId].electionCreator == msg.sender, "Only Election Creator Can Add Participants!");
     require(elections[_electionId].electionIsActive, "Election Periode Has Ended!");
-    require(elections[_electionId].electionParticipants[_voterAddress].voterWeight <= 0, "This User Already Added As Participant!");
-    require(elections[_electionId].electionParticipants[_voterAddress].voterVoted == false, "This User Already Voted!");
-    elections[_electionId].electionParticipants[_voterAddress].voterName = _voterName;
-    elections[_electionId].electionParticipants[_voterAddress].voterAddress = _voterAddress;
-    elections[_electionId].electionParticipants[_voterAddress].voterWeight = 1;
+    for (uint i = 0; i < _voterAddress.length; i++) {
+      if (
+        elections[_electionId].electionParticipants[_voterAddress[i]].voterWeight <= 0 &&
+        elections[_electionId].electionParticipants[_voterAddress[i]].voterVoted == false
+      ) {
+        elections[_electionId].electionParticipants[_voterAddress[i]].voterAddress = _voterAddress[i];
+        elections[_electionId].electionParticipants[_voterAddress[i]].voterWeight = 1;
+      }
+    }
   }
 
   /// Admin Stop Election Periode
@@ -104,7 +103,8 @@ contract Ballot {
   /// Registered User Participant Voting Some Candidate
   function vote(string memory _electionId, address _candidateAddress, address _voterAddress) public {
     require(elections[_electionId].electionIsActive, "Election Periode Has Ended!");
-    require(elections[_electionId].electionParticipants[_voterAddress].voterAddress == msg.sender, "This Is Not Your Ballot!");
+    require(_voterAddress == msg.sender, "This Is Not Your Ballot!");
+    require(elections[_electionId].electionParticipants[_voterAddress].voterWeight > 0, "You Are Not Registered!");
     require(elections[_electionId].electionParticipants[_voterAddress].voterVoted == false, "You Already Voted!");
     elections[_electionId].electionParticipants[_voterAddress].voterVoted = true;
     elections[_electionId].electionParticipants[_voterAddress].voterSelectedCandidate = _candidateAddress;
