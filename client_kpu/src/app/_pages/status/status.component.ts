6b270321331ca6587338be53bbfcef69212b471d
Bifeldy <bias.astho@gmail.com>
2020-06-17 01:48:40 +0900

    😥 Web API Error, Go Here => 13.67.36.4

--- client_kpu/src/app/_pages/status/status.component.ts
+++ client_kpu/src/app/_pages/status/status.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GlobalService } from 'src/app/_shared/services/global.service';
+
 @Component({
   selector: 'app-status',
   templateUrl: './status.component.html',
@@ -8,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class StatusComponent implements OnInit {
 
   constructor(
+    private gs: GlobalService
   ) {
   }
 
   ngOnInit() {
+    if (window.location.protocol === 'https:') {
+      this.gs.log(`[IFRAME-PROTOCOL] 'https' Detected, Changing To HTTP Instead.`);
+      window.location.protocol = 'http:';
+    }
   }
 
 }
